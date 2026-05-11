99711146bf85c1e555d1774fc604966f6862ed60
Aryan Chauhan <114977663+Aryanch9797@users.noreply.github.com>
2026-05-11 10:38:50 +0530

    Refine data transformations in FaceData class
    Updated data transformations for training and validation datasets, including normalization and augmentation adjustments.

--- src/Dataloader.py
+++ src/Dataloader.py
@@ -1,3 +1,5 @@
+""" Custom Dataset class for Training and Validation. """
+
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
@@ -5,28 +7,34 @@ from PIL import Image
 import os
 
 class FaceData(Dataset):
-    """ Custom Dataset class for Training and Validation. """
     
     def __init__(self, df, is_val, data_path="/kaggle/input/sep-25-dl-gen-ai-nppe-1/face_dataset/"):
         self.df = df
         self.data_path = data_path
         self.is_val = is_val
+
+        mean_ds = [0.477, 0.412, 0.380] 
+        std_ds  = [0.292, 0.273, 0.272] 
         
         val_transforms = [
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
+            transforms.Normalize(mean=mean_ds, std=std_ds)
         ]
-        
+
+  
+
         train_transforms = [
             transforms.Resize((224, 224)),
             transforms.RandomHorizontalFlip(p=0.3),    
-            transforms.RandomVerticalFlip(p=0.),
-            transforms.RandomRotation(degrees=15),            
+            transforms.RandomVerticalFlip(p=0.3),
+            transforms.RandomRotation(degrees=10),            
             transforms.ColorJitter(brightness=0.2,           
                                    contrast=0.2, 
                                    saturation=0.2, 
                                    hue=0.05),
             transforms.ToTensor(),
+            transforms.Normalize(mean=mean_ds, std=std_ds)
         ]
         
         if self.is_val:
@@ -47,8 +55,5 @@ class FaceData(Dataset):
         img = Image.open(full_img_path).convert('RGB')
         
         img_tensor = self.transform(img)
-        mean = img_tensor.mean()
-        std = img_tensor.std()
-        img_tensor_scaled = (img_tensor - mean) / (std + 1e-7) 
         
-        return img_tensor_scaled, torch.tensor(gender, dtype=torch.float32), torch.tensor(age, dtype=torch.float32)
+        return img_tensor, torch.tensor(gender, dtype=torch.float32), torch.tensor(age/100, dtype=torch.float32)
